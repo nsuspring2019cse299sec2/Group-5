b38c548e052448e9d5db7cd64ad987a777324872
mahedihassanpranto <hassan.pranto@northsouth.edu>
2019-03-02 13:38:51 +0600

    header to another page
    tried to go to another page after log out but it was failed

---
+++ PatientLoginForm.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Patient Login Form</title>
+    <link rel="stylesheet" href="loginform.css">
+</head>
+<body>
+   
+    <form action="">
+  <div class="imgcontainer">
+    <img src="image/img_avatar2.png" alt="" class="avatar">
+  </div>
+
+  <div class="container">
+    <label for="uname"><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="uname" required></br>
+
+    <label for="psw"><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="psw" required></form><br>
+        
+    <button type="submit">Login</button>
+    
+  </div>
+
+  <div class="container" style="background-color:#f1f1f1">
+    <button type="button" class="cancelbtn">Cancel</button>
+    <span class="psw">Forgot <a href="#">password?</a></span>
+  </div>
+</form>
+    
+</body>
+</html>
