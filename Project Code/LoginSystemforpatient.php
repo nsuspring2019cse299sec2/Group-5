3b768063fb464d6e8dd3c9af211b67ad6280a121
mahedihassanpranto <hassan.pranto@northsouth.edu>
2019-03-14 01:11:54 +0600

    project code folder
    every code is now in project code folder

--- Project Code/LoginSystemforpatient.php
+++ Project Code/LoginSystemforpatient.php
@@ -1,6 +1,6 @@
 <?php
 
-$con=mysqli_connect("localhost","root","","tutorial");
+$con=mysqli_connect("localhost","root","","find_doctor");
 if(!$con)
 {
     echo ("Error connection:".mysqli_connect_error());
@@ -8,10 +8,10 @@ if(!$con)
 
 if(isset($_POST['submit']))
 {
-    $username=$_POST['username'];
-    $password=$_POST['upassword'];
+    $username=$_POST['name'];
+    $password=$_POST['pasword'];
     
-    $sql="select * from Patient where username='$username' and password='$password'";
+    $sql="select * from Patient where name='$username' and pasword='$password'";
     $res=mysqli_query($con,$sql);
     $count=mysqli_num_rows($res);
     
