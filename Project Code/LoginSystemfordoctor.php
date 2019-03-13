9d67ace4735305ae77f6376202c04c7ed806bd9a
mahedihassanpranto <hassan.pranto@northsouth.edu>
2019-03-14 01:42:16 +0600

    login system for doctor
    login system is created for doctor so that they can go to home page after successful login

--- Project Code/LoginSystemfordoctor.php
+++ Project Code/LoginSystemfordoctor.php
@@ -1,6 +1,6 @@
 <?php
 
-$con=mysqli_connect("localhost","root","","tutorial");
+$con=mysqli_connect("localhost","root","","find_doctor");
 if(!$con)
 {
     echo ("Error connection:".mysqli_connect_error());
@@ -8,16 +8,16 @@ if(!$con)
 
 if(isset($_POST['submit']))
 {
-    $username=$_POST['username'];
-    $password=$_POST['upassword'];
+    $username=$_POST['name'];
+    $password=$_POST['pasword'];
     
-    $sql="select * from Doctor where username='$username' and password='$password'";
+    $sql="select * from doctor where name='$username' and pasword='$password'";
     $res=mysqli_query($con,$sql);
     $count=mysqli_num_rows($res);
     
     if($count==1)
     {
-        echo "Login success";
+        header("location:homepagefordoctor.php");
     }
     else
     {
